b694c769c9b6e9fd29a44d0ee0d92c2e92a58a30
Adam Smith <adam_smith3@comcast.com>
2021-03-13 23:33:56 -0500

    weave in backtest time

--- handler.py
+++ handler.py
@@ -22,7 +22,8 @@ def write_records(client, current_value, algorithm, env, portfolio, exchange, da
             current_time = time.mktime(date.timetuple())
         except Exception:
             date = datetime.strptime(backtest_time, '%Y-%m-%d %H:%M:%S')
-            current_time = time.mktime(date.timetuple())
+            current_time = str(time.mktime(date.timetuple()))
+
 
     dimensions = [
         {'Name': 'region', 'Value': 'us-east-1'},
