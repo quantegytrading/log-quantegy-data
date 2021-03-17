d490d77cb91d6699987c9b66ec944c0b06ef35e5
Adam Smith <adam_smith3@comcast.com>
2021-03-17 11:24:40 -0400

    Add percent value

--- handler.py
+++ handler.py
@@ -2,6 +2,7 @@
 import json
 
 import boto3
+# from requests_aws4auth import AWS4Auth
 from botocore.config import Config
 import time
 
@@ -10,12 +11,43 @@ def current_milli_time():
     return str(round(time.time() * 1000))
 
 
-def write_records(client, current_value, algorithm, env, portfolio_id, exchange, data_type, portfolio, backtest_time=None):
+# def write_es(client, current_value, algorithm, env, portfolio_id, exchange, data_type, portfolio, backtest_time=None):
+#     import boto3
+#
+#     host = ''  # For example, my-test-domain.us-east-1.es.amazonaws.com
+#     region = ''  # e.g. us-west-1
+#
+#     service = 'es'
+#     credentials = boto3.Session().get_credentials()
+#     awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
+#
+#     es = Elasticsearch(
+#         hosts=[{'host': host, 'port': 443}],
+#         http_auth=awsauth,
+#         use_ssl=True,
+#         verify_certs=True,
+#         connection_class=RequestsHttpConnection
+#     )
+#
+#     document = {
+#         "title": "Moneyball",
+#         "director": "Bennett Miller",
+#         "year": "2011"
+#     }
+#
+#     es.index(index="movies", doc_type="_doc", id="5", body=document)
+#
+#     print(es.get(index="movies", doc_type="_doc", id="5"))
+
+
+def write_records(client, current_value, algorithm, env, portfolio_id, exchange, data_type, portfolio,
+                      backtest_time=None):
+
     print("Writing records to " + env)
     current_time = current_milli_time()
 
     portfolioj = json.loads(portfolio)
-    percent_value = (((current_value/10000) * 100) -100)
+    percent_value = (((float(current_value)/10000.0) * 100.0) -100.0)
 
     dimensions = [
         {'Name': 'region', 'Value': 'us-east-1'},
